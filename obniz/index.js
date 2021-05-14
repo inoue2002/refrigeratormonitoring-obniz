b8a0c2e6823ab7688c5fc0eb9bbb8f21e00f8567
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2021-05-15 00:42:23 +0900

    何秒開いていたか計測結果をAPIに投げる

--- obniz/index.js
+++ obniz/index.js
@@ -1,7 +1,8 @@
 "use strict";
-
+require("dotenv").config();
 const Obniz = require("obniz");
-const obniz = new Obniz("69477898");
+const axios = require("axios");
+const obniz = new Obniz(process.env.OBNIZ_ID);
 
 obniz.onconnect = async () => {
   const hcsr04 = obniz.wired("HC-SR04", {
@@ -11,8 +12,8 @@ obniz.onconnect = async () => {
     vcc: 3,
   });
   let isOpen = false;
-  let startTime
-  let totalTime
+  let startTime;
+  let totalTime;
   while (true) {
     let avg = 0;
     let count = 0;
@@ -31,25 +32,28 @@ obniz.onconnect = async () => {
     console.log(avg, isOpen);
     if (avg > 100 && isOpen === false) {
       isOpen = true;
-      startTime = new Date().getTime()
+      startTime = new Date().getTime();
       console.log("冷蔵庫があきました！");
     }
     if (avg < 100 && isOpen === true) {
       //しまった時
       isOpen = false;
-      console.log( new Date().getTime() )
-      console.log(startTime)
+      console.log(new Date().getTime());
+      console.log(startTime);
 
-      totalTime = new Date().getTime() - startTime
-      totalTime = totalTime / 1000
+      totalTime = new Date().getTime() - startTime;
+      totalTime = totalTime / 1000;
       var hour = Math.floor(totalTime / 3600);
       var hour_wari = Math.floor(totalTime % 3600);
       var min = Math.floor(hour_wari / 60);
       var min_wari = Math.floor(hour_wari % 60);
       var sec = min_wari;
 
-      console.log(`${hour}時間${min}分${sec}秒開いていた`)
+      console.log(`${hour}時間${min}分${sec}秒開いていた`);
       //totalTimeをAPIに送る
+      axios.post(process.env.API_URL + "/register", {
+        totalTime: totalTime,
+      });
     }
     await obniz.wait(50);
   }
