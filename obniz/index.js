63fea907e7e813598d1f4959adc3d5adfd0618b2
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2021-05-15 00:22:01 +0900

    obniz

---
+++ obniz/index.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const Obniz = require("obniz");
+const obniz = new Obniz("69477898");
+
+obniz.onconnect = async () => {
+  const hcsr04 = obniz.wired("HC-SR04", {
+    gnd: 0,
+    echo: 1,
+    trigger: 2,
+    vcc: 3,
+  });
+  let isOpen = false;
+  let startTime
+  let totalTime
+  while (true) {
+    let avg = 0;
+    let count = 0;
+
+    for (let i = 0; i < 3; i++) {
+      // measure three time. and calculate average
+      const val = await hcsr04.measureWait();
+      if (val) {
+        count++;
+        avg += val;
+      }
+    }
+    if (count > 1) {
+      avg /= count;
+    }
+    console.log(avg, isOpen);
+    if (avg > 100 && isOpen === false) {
+      isOpen = true;
+      startTime = new Date().getTime()
+      console.log("冷蔵庫があきました！");
+    }
+    if (avg < 100 && isOpen === true) {
+      //しまった時
+      isOpen = false;
+      console.log( new Date().getTime() )
+      console.log(startTime)
+
+      totalTime = new Date().getTime() - startTime
+      totalTime = totalTime / 1000
+      var hour = Math.floor(totalTime / 3600);
+      var hour_wari = Math.floor(totalTime % 3600);
+      var min = Math.floor(hour_wari / 60);
+      var min_wari = Math.floor(hour_wari % 60);
+      var sec = min_wari;
+
+      console.log(`${hour}時間${min}分${sec}秒開いていた`)
+      //totalTimeをAPIに送る
+    }
+    await obniz.wait(50);
+  }
+};
