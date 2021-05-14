621d7182a29ac357a528c9b4eb6933c41256a4ce
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2021-05-15 00:11:48 +0900

    センサーの完成

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ obniz.onconnect = async () => {
     trigger: 2,
     vcc: 3,
   });
-
+  let isOpen = false;
+  let startTime
+  let totalTime
   while (true) {
     let avg = 0;
     let count = 0;
+
     for (let i = 0; i < 3; i++) {
       // measure three time. and calculate average
       const val = await hcsr04.measureWait();
@@ -25,7 +28,29 @@ obniz.onconnect = async () => {
     if (count > 1) {
       avg /= count;
     }
-    console.log(avg);
-    await obniz.wait(100);
+    console.log(avg, isOpen);
+    if (avg > 100 && isOpen === false) {
+      isOpen = true;
+      startTime = new Date().getTime()
+      console.log("冷蔵庫があきました！");
+    }
+    if (avg < 100 && isOpen === true) {
+      //しまった時
+      isOpen = false;
+      console.log( new Date().getTime() )
+      console.log(startTime)
+
+      totalTime = new Date().getTime() - startTime
+      totalTime = totalTime / 1000
+      var hour = Math.floor(totalTime / 3600);
+      var hour_wari = Math.floor(totalTime % 3600);
+      var min = Math.floor(hour_wari / 60);
+      var min_wari = Math.floor(hour_wari % 60);
+      var sec = min_wari;
+
+      console.log(`${hour}時間${min}分${sec}秒開いていた`)
+      //totalTimeをAPIに送る
+    }
+    await obniz.wait(50);
   }
 };
