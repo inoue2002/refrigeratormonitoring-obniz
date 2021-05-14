e3d5737c9ee8d445e872d93e876dcfda77300a92
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2021-05-15 04:02:52 +0900

    cronk関数

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@
 //Lambda関数
 
 const { Client } = require("@notionhq/client");
+const axios = require("axios");
+const querystring = require("querystring");
 // Initializing a client
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -10,19 +12,15 @@ const notion = new Client({
 exports.handler = async (e) => {
   console.log(e.path);
   console.log(e.body);
-  const event = JSON.parse(e.body);
+  const event = e.body;
   let message = "";
   switch (e.path) {
     case "/register":
       message = "ok";
-      //notionAPIを叩いて、今日の冷蔵庫が開いた回数と時間を取得する
-      await registerFunc(event);
+      await registerFunc(JSON.parse(event));
       break;
-    case "/result":
-      //cronで毎日24時に動く
-      //１日の結果をグループラインに送ってくれる
-      //notionの計測が初期化される。
-      //購入の必要があるものも教えてくれる
+    case "/cron":
+      await cronFunc();
       break;
   }
 
@@ -127,4 +125,76 @@ async function notionUpdateCount(todayCount, todayTime) {
     },
   };
   await notion.request(request_payload2);
+}
+
+async function cronFunc() {
+  //今のセンサーのあたいを取得しにいく
+  const request_payload = {
+    path: "databases/" + "fb9bd269-8888-4fa1-a07a-83817ce9ffb9" + "/query",
+    method: "POST",
+  };
+  const current_pages = await notion.request(request_payload);
+  let todayCount = {};
+  let todayTime = {};
+  for (const page of current_pages.results) {
+    if (page.id === "07ce4234-d2d3-43be-ba8d-18be7bec2b58") {
+      todayCount = { id: page.id, count: page.properties.count.number };
+    }
+    if (page.id === "9217cbb3-0a20-4fc0-9778-31d0002f200c") {
+      todayTime = { id: page.id, count: page.properties.count.number };
+    }
+  }
+  //今家で必要なものを取得しにいく
+  const request_payload2 = {
+    path: "databases/" + "c98d166221914e1a92f4e7a90761c0da" + "/query",
+    method: "POST",
+    body: {
+      filter: {
+        property: `want`,
+        checkbox: {
+          equals: true,
+        },
+      },
+    },
+  };
+  const current_pages2 = await notion.request(request_payload2);
+  let wantItems = "\n【現在この家に必要なものリスト】";
+  for (const item of current_pages2.results) {
+    wantItems =
+      wantItems + "\n" + "・" + item.properties.item.title[0].plain_text;
+  }
+
+  await axios.post(
+    "https://notify-api.line.me/api/notify",
+    querystring.stringify(
+      {
+        message: `本日は冷蔵庫が！${todayCount.count}回開けられ、開いていた時間は${todayTime.count}秒でした。明日はもっと短くなるように心がけましょう。`,
+      },
+    ),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${process.env.NOTIFY_TOKEN}`,
+      },
+    }
+  );
+  await axios.post(
+    "https://notify-api.line.me/api/notify",
+    querystring.stringify(
+      {
+        message: `${wantItems}\n編集:https://lin.ee/PgZiEkV`,
+      },
+    ),
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Bearer ${process.env.NOTIFY_TOKEN}`,
+      },
+    }
+  );
+
+  todayCount.count = 0;
+  todayTime.count = 0;
+  await notionUpdateCount(todayCount, todayTime);
+  //LINEに投げる
 }
